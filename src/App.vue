364b5c9081aacdcfa268616b544c8f29bb6fbc4e
jennityn <jenni.tynkkynen@metropolia.fi>
2022-04-28 15:47:24 +0300

    Navigointipalkki + yleistä toiminnallisuutta lisätty

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,193 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <h1>Vue todo app</h1>
+  <div class="navBar">
+    <button class="showDivs" @click="showDivAdd()">addaussivu</button>
+    &nbsp;&nbsp;&nbsp;
+    <button class="showDivs" @click="showDivAll()">kaikki tähän</button>
+  </div>
 
+
+  <div id="addDiv">
+    <form @submit.prevent="addNewTodo">
+      <div id="currentDate">Date: {{ currentDate() }}</div>
+      <label>TO-DO</label>
+      <input v-model="newToDo" type="text" placeholder="Add a new task" name="newTodo" maxlength="200">
+      <button>Add new todo</button>
+    </form>
+
+    <ul>
+      <li v-for="(todo, index) in todos" :key="todo.id" class="todo">
+        <div :class="{ done: todo.done }" @click="toggleDone(todo)">
+          {{ todo.content }}
+        </div>
+        <button class="removeItem" @click="removeToDo(index)">x</button>
+      </li>
+    </ul>
+
+    <button @click="markAllDone">Mark All Done</button>
+    &nbsp;&nbsp;&nbsp;
+    <button @click="removeAll">Remove All</button>
+  </div>
+
+  <div id="allDiv">
+    <div id="search">
+      <label for="startingDate">From:</label>
+      <input type="week" id="startingDate" name="startingDate">
+      <label for="endingDate">To:</label>
+      <input type="week" id="endingDate" name="endingDate">
+      <input @click="makeQuery()" type="button" value="Lähetä" id="submit">
+      <div id="divElement">
+
+      </div>
+    </div>
+  </div>
+
+
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+let json;
+
+import {ref} from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
+  name: 'App',
+  components: {},
+
+  setup() {
+    const newToDo = ref('');
+    const todos = ref([]);
+    const allTodos = ref([]);
+
+    function showDivAdd() {
+      let add = document.getElementById('addDiv');
+      let all = document.getElementById('allDiv');
+      add.style.display = 'block';
+      all.style.display = 'none';
+    }
+
+    function showDivAll() {
+      let add = document.getElementById('addDiv');
+      let all = document.getElementById('allDiv');
+      all.style.display = 'block';
+      add.style.display = 'none';
+    }
+
+    function currentDate() {
+      const current = new Date();
+      const date = current.getDate() + '-' + (current.getMonth() + 1) + '-' + current.getFullYear();
+      return date;
+    }
+
+    function addNewTodo() {
+      if (this.newToDo.trim().length == 0) {
+        return;
+      }
+      todos.value.push({
+        id: Date.now(), //!!!HUOM!!!! tähän databaseen menevä id-muoto
+        done: false,
+        content: newToDo.value,
+      });
+      newToDo.value = '';
+    }
+
+    function toggleDone(todo) {
+      todo.done = !todo.done;
+    }
+
+    function removeToDo(index) {
+      todos.value.splice(index, 1);
+    }
+
+    function markAllDone() {
+      todos.value.forEach((todo) => todo.done = true);
+    }
+
+    function removeAll() {
+      todos.value = [];
+    }
+
+    function makeQuery() {
+      let startdate = document.getElementById('startingDate').value;
+      let enddate = document.getElementById('endingDate').value;
+      if (startdate.length == 0) { // MUOKKAA TÄMÄ
+        return;
+      } else {
+        let xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+          if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            json = JSON.parse(xmlhttp.responseText);
+            console.log(json);
+
+            if (json.numOfRows > 0) { // something found
+              showList(json);
+            } else {
+              document.getElementById('locationInfo').innerHTML = '<br/>No tasks found within this period.';
+            }
+          }
+        };
+        xmlhttp.open('GET', 'http://localhost:8082/api/events?start=' + startdate + '&end=' + enddate, true);
+        xmlhttp.send();
+      }
+    }
+
+    function showList(json) {
+      //document.getElementById("locationInfo").innerHTML = "New text!";
+      let divElement = document.getElementById('divElement');
+
+      let i;
+      let unOrdered;
+      let listElement, nestedElement, unNestedElement;
+      let string;
+      for (i in json.rows) {
+        // create a form group div
+        unOrdered = document.createElement('ul');
+        unOrdered.setAttribute('class', 'del'); // mark all these dynamically created elements to be "deleted"
+        divElement.appendChild(unOrdered);
+
+        listElement = document.createElement('li');
+        listElement.setAttribute('class', 'del');
+        string = json.rows[i].Kuvaus;
+        listElement.innerHTML = string;
+        unOrdered.appendChild(listElement);
+        nestedElement = document.createElement('ul');
+        nestedElement.setAttribute('class', 'del');
+        listElement.appendChild(nestedElement);
+        unNestedElement = document.createElement('li');
+        unNestedElement.setAttribute('class', 'del');
+        string = json.rows[i].Paiva_id+ ', ' + json.rows[i].Kuvaus + ', ' + json.rows[i].Tehty;
+        unNestedElement.innerHTML = string;
+        nestedElement.appendChild(unNestedElement);
+      }
+    }
+
+    return {
+      todos,
+      newToDo,
+      allTodos,
+      addNewTodo,
+      toggleDone,
+      removeToDo,
+      markAllDone,
+      removeAll,
+      currentDate,
+      makeQuery,
+      showDivAll,
+      showDivAdd,
+    };
+  },
+  mounted() {
+    let add = document.getElementById('addDiv');
+    let all = document.getElementById('allDiv');
+    add.style.display = 'block';
+    all.style.display = 'none';
+  },
+};
+
+</script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+@import "app.css";
 </style>
---
+++ src/server.js
@@ -0,0 +1,193 @@
+let express = require('express');
+let app = express();
+let mysql = require('mysql');
+let util = require('util');
+let url = require('url');
+let bodyparser = require('body-parser');
+
+let urlencodedParser = bodyparser.urlencoded({extended: false});
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+/*app.use(function(req, res){
+  res.header("Access-Control_Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, ");
+  next();
+})*/
+
+let path = require('path');
+
+let con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'TipuTopakka92',
+  database: 'todo',
+});
+const query = util.promisify(con.query).bind(con);
+
+con.connect(function(err) {
+  if (err) throw err;
+  //con.query("SELECT * FROM event", function (err, result, fields) {
+  //if (err) throw err;
+  //console.log(result);
+  console.log('Connected');
+  //});
+});
+
+app.get('/api/events', function(req, res) {
+  console.log('Get tasks from a certain period');
+  let q = url.parse(req.url, true).query;
+  let params = q.start + ' ' + q.end;
+  let startDate = q.start;
+  let endDate = q.end;
+  let alteredResult;
+  let string;
+  console.log('Parametrit:' + startDate + ' ' + endDate);
+
+  let sql = 'SELECT event_date.Date, event.Name, event.Type, Location.Location_name'
+      + ' FROM event_date, event, location'
+      +
+      ' WHERE event_date.Event_id = event.Event_id and event.Location_Location_id = Location.Location_id'
+      + ' and event_date.Date >= ? and event_date.Date <= ?'
+      + ' GROUP BY Name'
+      + ' ORDER BY event_date.Date';
+
+  (async () => { // IIFE (Immediately Invoked Function Expression)
+    try {
+      const rows = await query(sql, [startDate, endDate]);
+      string = JSON.stringify(rows);
+      alteredResult = '{"numOfRows":' + rows.length + ',"rows":' + string + '}';
+      console.log(alteredResult);
+      console.log(rows);
+      res.send(alteredResult);
+
+    } catch (err) {
+      console.log('Database error!' + err);
+    } finally {
+      //conn.end();
+    }
+  })();
+});
+
+app.get('/events', function(req, res) {
+  res.sendFile(path.join(__dirname + '/server.html'));
+});
+
+app.get('api/location', function(req, res) {
+  console.log('Get location address');
+  let q = url.parse(req.url, true).query;
+  let params = q.locationName;
+  let location = q.locationName;
+  let alteredResult;
+  let string;
+  console.log('Parametrit:' + location);
+
+  let sql = 'SELECT Street_address, City, Zip, Country'
+      + ' FROM location'
+      +
+      ' WHERE Location_name = ?'
+      + ' GROUP BY Street_address'
+      + ' ORDER BY Street_address';
+
+  (async () => { // IIFE (Immediately Invoked Function Expression)
+    try {
+      const rows = await query(sql, [location]);
+      string = JSON.stringify(rows);
+      alteredResult = '{"numOfRows":' + rows.length + ',"rows":' + string + '}';
+      console.log(alteredResult);
+      console.log(rows);
+      res.send(alteredResult);
+
+    } catch (err) {
+      console.log('Database error!' + err);
+    } finally {
+      //conn.end();
+    }
+  })();
+});
+
+app.post('/api/testingevent', urlencodedParser, function(req, res) {
+  //console.log("Request body: " + req.body);
+  //console.log("Request body length: " + req.body.getLength);
+
+  console.log('body: %j', req.body);
+  // get JSON-object from the http-body
+  let jsonObj = req.body;
+  console.log('Arvo: ' + jsonObj.eventName);
+  // make updates to the database
+  let responseString = JSON.stringify(jsonObj);
+  res.send('POST successful: ' + responseString);
+});
+
+app.post('/api/event', urlencodedParser, function(req, res) {
+  let jsonObj = req.body;
+  let responseString = JSON.stringify(jsonObj);
+
+  if (jsonObj.eventLocation > -1) { // is  a location place already present?
+    let sql = 'INSERT INTO event (Name, Type, Location_location_id)'
+        + ' VALUES ( ?, ?, ?)';
+    (async () => {  // IIFE (Immediately Invoked Function Expression)
+      try {
+        const result = await query(sql, [
+          jsonObj.eventName,
+          jsonObj.eventType,
+          jsonObj.eventLocation]);
+
+        let insertedId = result.insertId;
+        sql = 'INSERT INTO Event_date (Date, Event_id)'
+            + ' VALUES ( ?, ?)';
+        await query(sql, [jsonObj.eventDate, insertedId]);
+        res.send('POST succesful ' + req.body);
+
+      } catch (err) {
+        console.log('Insertion into some (2) table was unsuccessful!' + err);
+        res.send('POST was not succesful ' + err);
+      }
+
+    })();
+
+  } else {
+
+    let sql = 'INSERT INTO location (Location_name, Street_address, City, Zip, Country)'
+        + ' VALUES (?, ?, ?, ?, ?)';
+    (async () => {
+      try {
+        const resultLocation = await query(sql, [
+          jsonObj.locPlaceName,
+          jsonObj.locStreetAddress,
+          jsonObj.locCity,
+          jsonObj.locZip,
+          jsonObj.locCountry]);
+
+        let insertedLocationId = resultLocation.insertId;
+        sql = 'INSERT INTO event (Name, Type, Location_location_id)'
+            + ' VALUES ( ?, ?, ?)';
+        const resultEvent = await query(sql, [
+          jsonObj.eventName,
+          jsonObj.eventType,
+          insertedLocationId]);
+
+        let insertedEventId = resultEvent.insertId;
+
+        sql = 'INSERT INTO Event_date (Date, Event_id)'
+            + ' VALUES ( ?, ?)';
+        await query(sql, [jsonObj.eventDate, insertedEventId]);
+
+        res.send('POST succesful ' + req.body);
+
+      } catch (err) {
+        console.log('Insertion into some (2) table was unsuccessful!' + err);
+        res.send('POST was not succesful ' + err);
+      }
+
+    })();
+  }
+});
+
+let server = app.listen(8082, function() {
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
